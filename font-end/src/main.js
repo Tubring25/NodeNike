7032395341263993322388ada7df5bd598e35305
sameen_25 <sameen9725@163.com>
2020-05-25 17:30:42 +0800

    font-end

--- font-end/src/main.js
+++ font-end/src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import axios from 'axios'
 
+Vue.use(ElementUI, axios)
 Vue.config.productionTip = false;
 
 new Vue({
