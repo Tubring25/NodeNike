0bad74864b85d6cf1e3785bf459ac27ccd13e61a
sameen_25 <sameen9725@163.com>
2020-06-04 17:33:08 +0800

    封装连接

--- back-end/db/db.js
+++ back-end/db/db.js
@@ -1,11 +1,26 @@
 const mysql = require('mysql');
 
-const conn = mysql.createConnection({
+const pool = mysql.createPool({
   host: 'localhost', //数据库地址
   user: 'root', //用户名
   port: '3306', // 端口号
   password: '123456', //密码
   database: 'nikesql' //数据库名
 });
-conn.connect();
-module.exports = conn;
+let db = {}
+
+db.query = function (sql, callback) {
+  if (!sql) {
+    callback();
+    return;
+  }
+  pool.query(sql, function (err, rows, fields) {
+    if (err) {
+      console.log(err);
+      callback(err, null);
+      return;
+    };
+    callback(null, rows, fields);
+  });
+}
+module.exports = db
---
+++ back-end/routes/cart.js
@@ -0,0 +1,12 @@
+/**
+ * 购物车接口
+ */
+var express = require('express');
+var router = express.Router();
+let conn = require('../db/db');
+let util = require('../util/util')
+
+// 添加购物车
+router.post('/addCart', (req, res) => {
+  
+})
