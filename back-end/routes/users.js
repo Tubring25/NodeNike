8750eb991909e066a59a70a581354662c856939d
Sameen <tubring25@gmail.com>
2020-06-15 07:20:47 +0800

    'x'

--- back-end/routes/users.js
+++ back-end/routes/users.js
@@ -24,7 +24,7 @@ router.get('/', function (req, res, next) {
 // 密码登录
 router.post('/login', (req, res) => {
 	let name = req.body.name;
-	let password = req.body.password
+	let password = req.body.password;
 	let sql = 'SELECT * from user WHERE name =? LIMIT 1 ;';
 	conn.query(sql, [name], (err, result) => {
 		if (err) {
@@ -33,9 +33,9 @@ router.post('/login', (req, res) => {
 			if (result[0]) {
 				if (result[0].password === password) {
 					res.cookie('user_id', result[0].id);
-					let response = result[0]
+					let response = result[0];
 
-					delete response.password
+					delete response.password;
 					res.json({
 						code: 0,
 						data: response
@@ -47,7 +47,7 @@ router.post('/login', (req, res) => {
 					});
 				}
 			} else {
-				res.json({ code: 2, msg: '用户不存在' })
+				res.json({ code: 2, msg: '用户不存在' });
 			}
 		}
 	});
@@ -56,7 +56,7 @@ router.post('/login', (req, res) => {
 // 注册
 router.post('/register', (req, res) => {
 	let sql2 = 'SELECT * FROM user WHERE phone=?';
-	let phone = req.body.phone
+	let phone = req.body.phone;
 
 	conn.query(sql2, [phone], (err, result) => {
 		if (err) {
@@ -78,86 +78,95 @@ router.post('/register', (req, res) => {
 					}
 				});
 			} else {
-				res.json({ code: 2, msg: "该手机号已被注册" })
+				res.json({ code: 2, msg: '该手机号已被注册' });
 			}
 		}
-	})
-})
+	});
+});
 // 更改个人信息
 router.post('/updateUser', function (req, res) {
-	let id = req.body.id
-	let keys = ['id', 'name', 'gender', 'email', 'phone']
+	let id = req.body.id;
+	let keys = ['id', 'name', 'gender', 'email', 'phone'];
 	for (let i in keys) {
 		if (req.body[keys[i]] == '' || req.body[keys[i]] == undefined || req.body[keys[i]] == null) {
-			res.json({ code: 1, msg: keys[i] + '不能为空' })
-			return
+			res.json({ code: 1, msg: keys[i] + '不能为空' });
+			return;
 		}
 	}
 	let searchSql = 'SELECT * from user WHERE id = ?';
 	conn.query(searchSql, [id], (err, result) => {
 		if (err) {
-			res.json({ code: 1, msg: err })
+			res.json({ code: 1, msg: err });
 		} else {
 			if (result.length == 0) {
-				res.json({ code: 1, msg: '用户不存在' })
+				res.json({ code: 1, msg: '用户不存在' });
 			} else {
-				let sql = `UPDATE user SET name = '` + req.body.name + `', gender = '` + req.body.gender + `', email = '` + req.body.email + `' WHERE id = '` + id + `'`;
+				let sql =
+					`UPDATE user SET name = '` +
+					req.body.name +
+					`', gender = '` +
+					req.body.gender +
+					`', email = '` +
+					req.body.email +
+					`' WHERE id = '` +
+					id +
+					`'`;
 				conn.query(sql, (err, result) => {
 					if (err) {
-						res.json({ code: 1, msg: err })
+						res.json({ code: 1, msg: err });
 					} else {
-						res.json({ code: 0, data: '更新成功' })
+						res.json({ code: 0, data: '更新成功' });
 					}
-				})
+				});
 			}
 		}
-	})
-})
+	});
+});
 // 更改密码
 router.post('/resetPwd', (req, res) => {
-	let keys = ['id', 'oldpwd', 'newpwd']
+	let keys = ['id', 'oldpwd', 'newpwd'];
 	for (let i in keys) {
 		if (req.body[keys[i]] == '' || req.body[keys[i]] == undefined || req.body[keys[i]] == null) {
-			res.json({ code: 1, msg: keys[i] + '不能为空' })
-			return
+			res.json({ code: 1, msg: keys[i] + '不能为空' });
+			return;
 		}
 	}
-	let searchSql = 'SELECT * FROM user WHERE id = ?'
+	let searchSql = 'SELECT * FROM user WHERE id = ?';
 	conn.query(searchSql, [req.body.id], (err, result) => {
 		if (err) {
-			res.json({ code: 1, msg: err })
+			res.json({ code: 1, msg: err });
 		} else {
 			if (result.length == 0) {
-				res.json({ code: 1, msg: '用户不存在' })
+				res.json({ code: 1, msg: '用户不存在' });
 			} else {
 				if (req.body.oldpwd === result[0].password) {
-					let sql = `UPDATE user SET password = '` + req.body.newpwd + `' WHERE id = ` + req.body.id
+					let sql = `UPDATE user SET password = '` + req.body.newpwd + `' WHERE id = ` + req.body.id;
 					conn.query(sql, (err2, result2) => {
 						if (err2) {
-							res.json({ code: 1, msg: data })
+							res.json({ code: 1, msg: data });
 						} else {
-							res.json({ code: 0, data: '密码修改成功' })
+							res.json({ code: 0, data: '密码修改成功' });
 						}
-					})
+					});
 				} else {
-					res.json({ code: 1, msg: '密码校验失败' })
+					res.json({ code: 1, msg: '密码校验失败' });
 				}
 			}
 		}
-	})
-})
+	});
+});
 // 添加购物车
 router.post('/addCart', (req, res) => {
-	let uid = Number(req.body.uid)
-	let gid = Number(req.body.gid)
-	let num = req.body.num
-	let goods_title = req.body.goods_title
-	let goods_img = req.body.goods_img
-	let price = req.body.price
+	let uid = Number(req.body.uid);
+	let gid = Number(req.body.gid);
+	let num = req.body.num;
+	let goods_title = req.body.goods_title;
+	let goods_img = req.body.goods_img;
+	let price = req.body.price;
 	let searchSql = 'SELECT * FROM cart WHERE gid=' + gid + ' AND user_id=' + uid;
 	conn.query(searchSql, (err, result) => {
 		if (err) {
-			res.json({ code: 1, msg: err })
+			res.json({ code: 1, msg: err });
 		} else {
 			if (result.length == 0) {
 				let addSql = 'INSERT INTO cart(gid, user_id, num, goods_title, goods_img, price) values(?,?,?,?,?,?)';
@@ -169,79 +178,95 @@ router.post('/addCart', (req, res) => {
 					}
 				});
 			} else {
-				let updateSql = 'UPDATE cart SET num = ?, price = ? WHERE gid = ? AND user_id = ?'
+				let updateSql = 'UPDATE cart SET num = ?, price = ? WHERE gid = ? AND user_id = ?';
 				conn.query(updateSql, [num, price, gid, uid], (err, updateRes) => {
 					if (err) {
-						res.json({ code: 1, msg: err })
+						res.json({ code: 1, msg: err });
 					} else {
-						res.json({ code: 0, data: '更新成功' })
+						res.json({ code: 0, data: '更新成功' });
 					}
-				})
+				});
 			}
 		}
-	})
-})
+	});
+});
 // 获取购物车
 router.post('/getShopCart', (req, res) => {
-	let uid = req.body.uid
-	let searchSql = 'SELECT * FROM cart WHERE user_id = ?'
+	let uid = req.body.uid;
+	let searchSql = 'SELECT * FROM cart WHERE user_id = ?';
 	conn.query(searchSql, [uid], (err, result) => {
 		if (err) {
-			res.json({ code: 1, msg: err })
+			res.json({ code: 1, msg: err });
 		} else {
-			res.json({ code: 0, data: result })
+			res.json({ code: 0, data: result });
 		}
-	})
-})
+	});
+});
 // 购物车删除
 router.post('/deleteCart', (req, res) => {
-	let uid = req.body.uid
-	let gid = req.body.gid
-	let sql = 'DELETE FROM cart WHERE user_id=? AND gid=?'
+	let uid = req.body.uid;
+	let gid = req.body.gid;
+	let sql = 'DELETE FROM cart WHERE user_id=? AND gid=?';
 	conn.query(sql, [uid, gid], (err, result) => {
 		if (err) {
-			res.json({ code: 1, msg: err })
+			res.json({ code: 1, msg: err });
 		} else {
-			res.json({ code: 0, data: '删除成功' })
+			res.json({ code: 0, data: '删除成功' });
 		}
-	})
-})
+	});
+});
 // 下单
 router.post('/buy', (req, res) => {
-	let uid = req.body.uid
-	let date = new Date()
-	let order_time = date.getFullYear() +'-'+(date.getMonth()+1) +'-'+date .getDate();
-	let num = req.body.num
-	let address = req.body.address
-	let phone = req.body.phone
-	let order_name = req.body.order_name
-	let goods = req.body.goods
-	let is_finish = '0'
-	let sql = 'INSERT INTO orderTable(user_id,order_time, num, address, phone, order_name, is_finish, goods) values (?,?,?,?,?,?,?,?)'
-	conn.query(sql, [uid,order_time, num, address, phone, order_name, is_finish, goods], (err, reslut) =>{
+	let uid = req.body.uid;
+	let date = new Date();
+	let order_time = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
+	let num = req.body.num;
+	let address = req.body.address;
+	let phone = req.body.phone;
+	let order_name = req.body.order_name;
+	let goods = req.body.goods;
+	let is_finish = '0';
+	let sql =
+		'INSERT INTO orderTable(user_id,order_time, num, address, phone, order_name, is_finish, goods) values (?,?,?,?,?,?,?,?)';
+	conn.query(sql, [uid, order_time, num, address, phone, order_name, is_finish, goods], (err, reslut) => {
 		if (err) {
-			res.json({ code: 1, msg: err })
+			res.json({ code: 1, msg: err });
 		} else {
-			res.json({ code: 0, data: '下单成功' })
+			res.json({ code: 0, data: '下单成功' });
 		}
-	})
-})
+	});
+});
 // 获取订单列表
 router.post('/getOrder', (req, res) => {
-	let uid = req.body.uid
-	let sql = ''
+	let uid = req.body.uid;
+	let sql = '';
 	if (uid) {
-		sql = 'SELECT * FROM orderTable WHERE user_id=' + uid
+		sql = 'SELECT * FROM ordertable WHERE user_id=' + uid;
 	} else {
-		sql = 'SELECT * FROM orderTable'
+		sql = 'SELECT * FROM ordertable';
 	}
 	conn.query(sql, (err, result) => {
-		if(err) {
-			res.json({code:1, msg: err})
+		if (err) {
+			res.json({ code: 1, msg: err });
 		} else {
-			res.json({code:0, data: result})
+			// for (const i in result) {
+			// 	// console.log(result[i].goods.split(','))
+			// 	// return
+			// 	let goods = result[i].goods.split(',');
+			// 	for (const j in goods) {
+			// 		let searchSql = 'SELECE * FROM orderTable a, goods b WHERE b.id = ?';
+			// 		conn.query(searchSql, [Number(goods[j])], (err2, result2) => {
+			// 			if (err2) {
+			// 				res.json({ code: 1, msg: '获取关联商品失败' });
+			// 			} else {
+			// 				result[i].goods = result2;
+			// 			}
+			// 		});
+			// 	}
+			// }
+			res.json({ code: 0, data: result });
 		}
-	})
-})
+	});
+});
 
 module.exports = router;
--- font-end/src/views/myOrder.vue
+++ font-end/src/views/myOrder.vue
@@ -2,39 +2,50 @@
   <div class="container">
     <navBar></navBar>
     <div class="content">
-      
+      <el-table :data="tableData" style="width: 100%">
+        <el-table-column prop="id" label="ID" width="180"></el-table-column>
+        <el-table-column prop="order_time" label="下单时间" width="180"></el-table-column>
+        <el-table-column prop="order_name" label="收件人姓名" width="180"></el-table-column>
+        <el-table-column prop="phone" label="收件人电话" width="180"></el-table-column>
+        <el-table-column prop="address" label="收件人地址" width="180"></el-table-column>
+        <el-table-column prop="is_finish" label="订单状态" width="180">
+          <el-tag>进行中</el-tag>
+          <el-tag type="success">已完成</el-tag>
+          <el-tag type="danger">已取消</el-tag>
+        </el-table-column>
+      </el-table>
     </div>
   </div>
-  
 </template>
 <script>
 import navBar from "../components/nav";
-import {getOrder} from '../api/order'
+import { getOrder } from "../api/order";
 export default {
   components: { navBar },
-  data(){
+  data() {
     return {
-      orderList: [], // 订单列表
-    }
+      orderList: [] // 订单列表
+    };
   },
   created() {
     if (!JSON.parse(localStorage.getItem("userInfo"))) {
       this.$message.info("请先登录");
       return;
     }
-    this.getOrder_()
+    this.getOrder_();
   },
   methods: {
     getOrder_() {
-      getOrder({uid: JSON.parse(localStorage.getItem("userInfo")).id}).then(res=>{
-        if(res.data.code == 0) {
-          this.orderList = res.data.data
-        } else {
-          this.$message.error(res.data.msg)
+      getOrder({ uid: JSON.parse(localStorage.getItem("userInfo")).id }).then(
+        res => {
+          if (res.data.code == 0) {
+            this.orderList = res.data.data;
+          } else {
+            this.$message.error(res.data.msg);
+          }
         }
-      })
+      );
     }
-  },
-
-}
+  }
+};
 </script>
