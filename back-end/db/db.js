9ffead29164cc04030c906bd174cc2db7338faa1
sameen_25 <sameen9725@163.com>
2020-06-20 17:22:53 +0800

    改成连接池

--- back-end/db/db.js
+++ back-end/db/db.js
@@ -1,11 +1,20 @@
 const mysql = require('mysql');
 
-const conn = mysql.createConnection({
+// const conn = mysql.createConnection({
+// 	host: 'localhost', //数据库地址
+// 	user: 'root', //用户名
+// 	port: '3306', // 端口号
+// 	password: '123456', //密码
+// 	database: 'nikesql' //数据库名
+// });
+// conn.connect();
+const conn = mysql.createPool({
+	connectionLimit: 10, // 最大连接数
 	host: 'localhost', //数据库地址
 	user: 'root', //用户名
 	port: '3306', // 端口号
 	password: '123456', //密码
 	database: 'nikesql' //数据库名
 });
-conn.connect();
+
 module.exports = conn;
