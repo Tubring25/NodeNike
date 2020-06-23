2abd694ea074e8c3e331d30253185c0077c1511f
sameen_25 <sameen9725@163.com>
2020-06-23 17:29:43 +0800

    重构back-end

---
+++ back-end/config/db.js
@@ -0,0 +1,20 @@
+import Sequelize from 'sequelize'
+const config = {
+	database: 'nikesql',
+	user: 'root',
+	password: '123456'
+}
+const sequelize = new Sequelize(config['database'], config['user'], config['password'], {
+	dialect: 'mysql',
+	pool: {
+		max: 5, 
+		min: 0,
+		idea: 10000
+	}
+})
+sequelize.authenticate().then(()=>{
+  console.log('已成功连接服务器')
+}).catch(()=>{
+  console.error('连接服务器失败')
+})
+module.exports = sequelize
