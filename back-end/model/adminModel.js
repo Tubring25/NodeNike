567363d620b245dcdf5d473f22ee189ff3575f3d
Sameen <tubring25@gmail.com>
2020-06-25 22:59:51 +0800

    'rebuild'

--- back-end/model/adminModel.js
+++ back-end/model/adminModel.js
@@ -1,3 +1,11 @@
 import BaseModel from './baseModel'
+import Sequelize from 'sequelize'
 
-// class 
+class AdminModel extends BaseModel{
+  constructor() {
+    super('admin', {
+      username: {type: Sequelize.STRING},
+      
+    })
+  }
+}
