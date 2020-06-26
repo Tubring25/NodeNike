08a54050ef4ca80ed438e999baec407416640361
Sameen <tubring25@gmail.com>
2020-06-26 22:47:17 +0800

    'rebuldBackEnd'

--- back-end/model/adminModel.js
+++ back-end/model/adminModel.js
@@ -5,7 +5,12 @@ class AdminModel extends BaseModel{
   constructor() {
     super('admin', {
       username: {type: Sequelize.STRING},
-      
+      password: {type: Sequelize.STRING},
+      email: {type: Sequelize.STRING},
+      mobile: {type: Sequelize.STRING}
     })
+    this.model = super.getModel()
+    this.model.sync()
   }
 }
+module.exports = new AdminModel()
