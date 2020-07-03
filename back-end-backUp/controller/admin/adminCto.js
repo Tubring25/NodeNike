265a50dfce042447bc941ab91e2626659fe22cd0
Sameen <tubring25@gmail.com>
2020-07-03 22:50:16 +0800

    'x'

--- back-end-backUp/controller/admin/adminCto.js
+++ back-end-backUp/controller/admin/adminCto.js
@@ -1,14 +1,32 @@
 const adminModel = require('../../model/adminModel');
+const cryptp = require('crypto');
 
 class adminService {
-	constructor(){
+	constructor() {
 		this.instance = require('../../model/adminModel');
 	}
 	findAll() {
 		return this.instance.findAll();
 	}
-	create() {
-		
+	login(body) {
+		try {
+			if (!body.username) {
+				return {code: 0, data: '用户名错误'}
+			} else if (!body.password) {
+				return { code: 0, data: '密码错误' };
+			}
+		} catch (err) {
+			console.log(err.message, err);
+			return { code: 0, type: 'GET_ERROR_PARAM', data: err.message };
+		}
+	}
+	encryption(password) {
+		const newpassword = this.Md5(this.Md5(password).substr(2, 7) + this.Md5(password));
+		return newpassword;
+	}
+	Md5(password) {
+		const md5 = crypto.createHash('md5');
+		return md5.update(password).digest('base64');
 	}
 }
 module.exports = new adminService();
--- back-end-backUp/routes/admin.js
+++ back-end-backUp/routes/admin.js
@@ -5,5 +5,7 @@ const router = express.Router()
 router.get('/all', async (req, res, next) => {
   res.json(await Admin.findAll())
 });
-
+router.post('/login', async(req, res, next) => {
+  res.json(await Admin.login(req.body))
+})
 module.exports = router;
