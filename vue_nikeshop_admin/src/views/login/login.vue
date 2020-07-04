a6a4145f98ac1caa2f01f87b6c184b0d034935a0
Sameen <sameen9725@163.com>
2020-07-04 18:02:09 +0800

    create login.vue

--- vue_nikeshop_admin/src/views/login/login.vue
+++ vue_nikeshop_admin/src/views/login/login.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="login-container">
-    <el-form ref="login-from" :model="loginFrom" :rules="loginRules"></el-form>
+    <el-form ref="login-form" :model="loginFrom" :rules="loginRules">
+      <h3 class="title">Login</h3>
+      <el-form-item prop="username">
+        <i class="el-icon-user"></i>
+        <el-input class="input" v-model="loginFrom.username" placeholder="Username" tabindex="1" type="text" />
+      </el-form-item>
+      <el-form-item prop="password">
+        <i class="el-icon-lock"></i>
+        <el-input class="input" v-model="loginFrom.password" placeholder="password" tabindex="1" type="password" />
+      </el-form-item>
+      <el-form-item></el-form-item>
+    </el-form>
   </div>
 </template>
 <script>
