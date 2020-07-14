215e6be4894e1ac5502c6ad4ba9382d7e82887ba
Sameen <tubring25@gmail.com>
2020-07-14 22:21:46 +0800

    'Nothing'

--- vue_nikeshop_admin/src/layout/components/navBar.vue
+++ vue_nikeshop_admin/src/layout/components/navBar.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="nav">
-<<<<<<< HEAD
     <el-breadcrumb class="app-breadcrumb" separator="/">
       <transition-group name="breadcrumb">
         <el-breadcrumb-item v-for="item in levelList" :key="item.path">
@@ -15,19 +14,6 @@
         
       </el-dropdwn> -->
     </div>
-=======
-    <el-breadcrumb separator="/">
-      <span v-for="item in pathList" :key="item.path">
-        <el-breadcrumb-item><a :href="item.path">{{item.meta.title}}</a></el-breadcrumb-item>
-      </span>
-    </el-breadcrumb>
-    <el-breadcrumb separator="/" >
-      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-      <el-breadcrumb-item></el-breadcrumb-item>
-      <el-breadcrumb-item>活动列表</el-breadcrumb-item>
-      <el-breadcrumb-item>活动详情</el-breadcrumb-item>
-    </el-breadcrumb>
->>>>>>> 73cf562f70ddaf5baa73647fdab69950c94db8a4
   </div>
 </template>
 <script>
@@ -35,45 +21,16 @@ import {mapGetters} from 'vuex';
 export default {
   data() {
     return {
-<<<<<<< HEAD
-      levelList: []
-    };
-  },
-  created() {
-    this.getBread();
-    console.log(this.avatar)
-  },
-  computed: {
-    ...mapGetters([
-      'avatar',
-    ])
-  },
-  watch: {
-    $route: {
-      handler: function() {
-        this.getBread()
-      },
-      // 深度观察监听
-      deep: true
-=======
-      pathList: [],
+      levelList: [],
     }
   },
   created() {
     this.getBread()
   },
-  methods: {
-    getBread(){
-      let matched = this.$route.matched.filter(item => item.meta && item.meta.title)
-      const first = matched[0]
-      if(first.name.trim().toLocaleLowerCase() !== 'dashboard') {
-        matched = [{path: '/dashboard', meta: {title: '首页'}}].concat(matched)
-      }
-      this.pathList = matched
-      console.log(matched)
-
->>>>>>> 73cf562f70ddaf5baa73647fdab69950c94db8a4
-    }
+  computed: {
+    ...mapGetters([
+      "avatar"
+    ])
   },
   methods: {
     getBread() {
