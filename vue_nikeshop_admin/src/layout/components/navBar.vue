e8b890e545fd8270b065c79cccb0ef79a53bb7f5
Sameen <sameen9725@163.com>
2020-07-15 11:18:23 +0800

    commit temp

--- vue_nikeshop_admin/src/layout/components/navBar.vue
+++ vue_nikeshop_admin/src/layout/components/navBar.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="nav">
-<<<<<<< HEAD
     <el-breadcrumb class="app-breadcrumb" separator="/">
       <transition-group name="breadcrumb">
         <el-breadcrumb-item v-for="item in levelList" :key="item.path">
@@ -15,19 +14,6 @@
         
       </el-dropdwn> -->
     </div>
-=======
-    <el-breadcrumb separator="/">
-      <span v-for="item in pathList" :key="item.path">
-        <el-breadcrumb-item><a :href="item.path">{{item.meta.title}}</a></el-breadcrumb-item>
-      </span>
-    </el-breadcrumb>
-    <el-breadcrumb separator="/" >
-      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-      <el-breadcrumb-item></el-breadcrumb-item>
-      <el-breadcrumb-item>活动列表</el-breadcrumb-item>
-      <el-breadcrumb-item>活动详情</el-breadcrumb-item>
-    </el-breadcrumb>
->>>>>>> 73cf562f70ddaf5baa73647fdab69950c94db8a4
   </div>
 </template>
 <script>
@@ -35,7 +21,6 @@ import {mapGetters} from 'vuex';
 export default {
   data() {
     return {
-<<<<<<< HEAD
       levelList: []
     };
   },
@@ -55,24 +40,6 @@ export default {
       },
       // 深度观察监听
       deep: true
-=======
-      pathList: [],
-    }
-  },
-  created() {
-    this.getBread()
-  },
-  methods: {
-    getBread(){
-      let matched = this.$route.matched.filter(item => item.meta && item.meta.title)
-      const first = matched[0]
-      if(first.name.trim().toLocaleLowerCase() !== 'dashboard') {
-        matched = [{path: '/dashboard', meta: {title: '首页'}}].concat(matched)
-      }
-      this.pathList = matched
-      console.log(matched)
-
->>>>>>> 73cf562f70ddaf5baa73647fdab69950c94db8a4
     }
   },
   methods: {
@@ -106,10 +73,16 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-.app-breadcrumb {
-  width: 300px;
-  height: 50px;
-  line-height: 50px;
-  padding-left: 15px;
+.nav {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  .app-breadcrumb {
+    width: 300px;
+    height: 50px;
+    line-height: 50px;
+    padding-left: 15px;
+  }
 }
+
 </style>
