c56857504780a90c70795a30f592b4543754bb56
Sameen <tubring25@gmail.com>
2020-09-02 21:03:27 +0800

    新建商品页

--- vue_nikeshop_admin/src/views/goods/addGoods.vue
+++ vue_nikeshop_admin/src/views/goods/addGoods.vue
@@ -2,7 +2,38 @@
   <div class="add-goods-container">
     <!-- 商品名称 -->
     <el-form :model="goodsInfo" :rules="rules"  label-width="100px">
-      <!-- <el-form- -->
+      <el-form-item label="商品名称" prop="title">
+        <el-input v-model="goodsInfo.title" maxlength="50"></el-input>
+      </el-form-item>
+      <el-form-item label="商品类型">
+          <el-select v-model="goodsInfo.base_type_id" placeholder="请选择">
+            <el-option v-for="item in goodsBaseType" :key="item.id" :label="item.name" :value="item.id"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="性别">
+          <el-select v-model="goodsInfo.gender_id" placeholder="请选择">
+            <el-option label="男女同款" value="10"></el-option>
+            <el-option label="女款" value="0"></el-option>
+            <el-option label="男款" value="1"></el-option>
+            <el-option label="儿童" value="2"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="品牌">
+          <el-select v-model="goodsInfo.brand_id" placehoder="请选择品牌">
+            <el-option v-for="item in goodsBrandsType" :key="item.id" :label="item.name" :value="item.id"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="运动">
+          <el-select v-model="goodsInfo.sports_id" placehoder="请选择品牌">
+            <el-option v-for="item in goodsSportsType" :key="item.id" :label="item.name" :value="item.id"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="是否打折">
+          <el-select v-model="goodsInfo.is_onsale" placeholder="请选择">
+            <el-option label="是" value="1"></el-option>
+            <el-option label="否" value="0"></el-option>
+          </el-select>
+        </el-form-item>
     </el-form>
     <!-- 基础分类 -->
     <!-- 性别属性 -->
@@ -19,6 +50,7 @@
   </div>
 </template>
 <script>
+import {getGoodsType} from '@/api/goods'
 export default {
   data(){
     return {
@@ -31,7 +63,32 @@ export default {
           { required: true, message: '请输入活动描述', trigger: 'blur' },
         ],
       },
+      goodsBaseType: [],
+      goodsSportsType: [],
+      goodsBrandsType: [],
     }
-  }
+  },
+  created() {
+    this.getAllTypes()
+  },
+  methods: {
+    getAllTypes(){
+      getGoodsType({type:1, pageSize: 50, page: 1}).then(res=>{
+        if(res.code == 1) {
+          this.goodsBaseType = res.data
+        }
+      })
+      getGoodsType({type:2, pageSize: 50, page: 1}).then(res=>{
+        if(res.code == 1) {
+          this.goodsSportsType = res.data
+        }
+      })
+      getGoodsType({type:3, pageSize: 50, page: 1}).then(res=>{
+        if(res.code == 1) {
+          this.goodsBrandsType = res.data
+        }
+      })
+    },
+  },
 }
 </script>
