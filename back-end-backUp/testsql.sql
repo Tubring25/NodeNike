ef67fb296ee799a7293c61622b46eb748a45d45d
Sameen <sameen9725@163.com>
2020-09-02 16:51:10 +0800

    更新数据表数据

--- back-end-backUp/testsql.sql
+++ back-end-backUp/testsql.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80020
  File Encoding         : 65001
 
- Date: 20/07/2020 17:55:20
+ Date: 02/09/2020 16:49:56
 */
 
 SET NAMES utf8mb4;
@@ -39,6 +39,32 @@ CREATE TABLE `admins`  (
 INSERT INTO `admins` VALUES (1, 'admin', '123456', '18364477711', '81111@qq.com', '2020-07-08 00:00:00', 0, 'https://tvax4.sinaimg.cn/crop.0.0.379.379.180/007jermFly8gc8p1lhj1gj30aj0ajq3t.jpg?KID=imgbed,tva&Expires=1594180975&ssig=dXHSa3SrfK');
 
 -- ----------------------------
+-- Table structure for banner
+-- ----------------------------
+DROP TABLE IF EXISTS `banner`;
+CREATE TABLE `banner`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `desc` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `gender` int(0) NULL DEFAULT NULL,
+  `imgUrl` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `relate_type` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `relate_goods` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `is_top` tinyint(1) NULL DEFAULT NULL,
+  `createdAt` datetime(0) NOT NULL,
+  `updatedAt` datetime(0) NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 12 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of banner
+-- ----------------------------
+INSERT INTO `banner` VALUES (6, '机能风出没', '一步飞跃,引领街头时尚潮流', 10, '1597632112691_85.jpg', NULL, NULL, 1, '2020-08-17 02:41:53', '2020-08-17 02:41:53');
+INSERT INTO `banner` VALUES (7, 'NIKE AIR VAPORMAX 2020 FLYKNIT', '.', 1, '1597632210946_869.jpg', NULL, NULL, 1, '2020-08-17 02:43:59', '2020-08-17 02:43:59');
+INSERT INTO `banner` VALUES (8, 'NIKE AIR VAPORMAX 2020 FLYKNIT.', '.', 0, '1597636113840_567.jpg', NULL, NULL, 1, '2020-08-17 03:48:42', '2020-08-17 03:48:51');
+INSERT INTO `banner` VALUES (9, '开学季,穿新装', '.', 2, '1597636188590_478.jpg', NULL, NULL, 1, '2020-08-17 03:49:49', '2020-08-17 03:49:49');
+
+-- ----------------------------
 -- Table structure for goods_base_type
 -- ----------------------------
 DROP TABLE IF EXISTS `goods_base_type`;
@@ -50,7 +76,7 @@ CREATE TABLE `goods_base_type`  (
   `type` int(0) NULL DEFAULT NULL,
   `is_delete` tinyint(1) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 24 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of goods_base_type
@@ -112,7 +138,7 @@ CREATE TABLE `goods_sports_type`  (
   `gender` int(0) NULL DEFAULT NULL,
   `is_delete` tinyint(1) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of goods_sports_type
@@ -125,5 +151,27 @@ INSERT INTO `goods_sports_type` VALUES (5, '网球', NULL, 0, 0);
 INSERT INTO `goods_sports_type` VALUES (6, '健身/训练', NULL, 0, 0);
 INSERT INTO `goods_sports_type` VALUES (7, '瑜伽', NULL, 0, 0);
 INSERT INTO `goods_sports_type` VALUES (8, '高尔夫', NULL, 0, 0);
+
+-- ----------------------------
+-- Table structure for story
+-- ----------------------------
+DROP TABLE IF EXISTS `story`;
+CREATE TABLE `story`  (
+  `storyId` int(0) NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `desc` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `type` int(0) NULL DEFAULT NULL,
+  `imgUrl` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `relate_type` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `relate_goods` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `is_top` tinyint(1) NULL DEFAULT NULL,
+  `createdAt` datetime(0) NOT NULL,
+  `updatedAt` datetime(0) NOT NULL,
+  PRIMARY KEY (`storyId`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of story
+-- ----------------------------
 
 SET FOREIGN_KEY_CHECKS = 1;
