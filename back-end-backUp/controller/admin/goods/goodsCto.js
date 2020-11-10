b559f686334bef14888f1ba53de3d5430f440451
Sameen <sameen9725@163.com>
2020-11-10 17:33:53 +0800

    上传

--- back-end-backUp/controller/admin/goods/goodsCto.js
+++ back-end-backUp/controller/admin/goods/goodsCto.js
@@ -1,5 +1,4 @@
 const goodsSpuModule = require('../../../model/goods/goodsSpu');
-const commonService = require('../common/common');
 const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
 const fs = require('fs');
@@ -10,50 +9,52 @@ const path = require("path");
 
 class goodsService {
   async addSkuImg(req, res) {
-    console.log(req.file == undefined)
+    console.log(req)
+    let reqInfo = req    
     let that = this
-    that.getFileName(req).then(name => {
-      that.createFolder('public/goods/' + name).then(res => {
-        let form = new formiable.IncomingForm();
-        form.encoding = "utf8";
+    let form = new formiable.IncomingForm();
+    form.encoding = "utf8";
+    form.keepExtensions = true; // 是否包括 扩展名
+    form.maxFieldsSize = 4 * 1024 * 1024; // 最大字节数   
+    form.parse(req, (err, fields, files) => {
+      let goodsId = fields.tempId
+      // if (!fs.existsSync('public/goods/' + goodsId.replace("\"", "").replace("\"", ""))) {
+      //   fs.mkdirSync('public/goods/' + goodsId.replace("\"", "").replace("\"", ""));
+      // }
+      // let uploadPath = path.join(__dirname, "../../../public/goods/" + goodsId.replace("\"", "").replace("\"", ""));
+      form.uploadDir = path.join(__dirname, "../../../public/goods/NK01604972960394214");
+      let file = files.file;
+      if (err) {
+        return ("服务器错误" + err, null);
+      }
+      if (file.size > form.maxFileSize) {
+        fs.unlink(file.path);
+        return ("图片不得超过3M", null);
+      }
 
-        form.uploadDir = path.join(__dirname, "../../../public");
-        form.keepExtensions = true; // 是否包括 扩展名
-        form.maxFieldsSize = 4 * 1024 * 1024; // 最大字节数
+      let extName = "";
+      if (file.type == "image/png" || file.type == "image/x-png") {
+        extName = "png";
+      } else if (file.type == "image/jpg" || file.type == "image/jpeg") {
+        extName = "jpg";
+      }
+      if (extName.length == 0) {
+        return ("只支持png与jpg格式的图片", null);
+      }
+      console.log(file)
+      let timestamp = Number(new Date());
+      let num = Math.floor(Math.random() * 1000);
+      let imageName = `${timestamp}_${num}.${extName}`;
+      let newPath = form.uploadDir + "/" + imageName;
+      fs.rename(file.path, newPath, (err) => {
+        if (err) {
+          return (err, null);
+        } else {
+          return (null, newPath);
+        }
+      });
 
-        form.parse(req, (err, fields, files) => {
-          let file = files.file;
-          if (err) {
-            return("服务器错误" + err, null);
-          }
-          if (file.size > form.maxFileSize) {
-            fs.unlink(file.path);
-            return("图片不得超过3M", null);
-          }
-
-          let extName = "";
-          if (file.type == "image/png" || file.type == "image/x-png") {
-            extName = "png";
-          } else if (file.type == "image/jpg" || file.type == "image/jpeg") {
-            extName = "jpg";
-          }
-          if (extName.length == 0) {
-            return("只支持png与jpg格式的图片", null);
-          }
-          let timestamp = Number(new Date());
-          let num = Math.floor(Math.random() * 1000);
-          let imageName = `${timestamp}_${num}.${extName}`;
-          let newPath = form.uploadDir + "/" + imageName;
-          fs.rename(file.path, newPath, (err) => {
-            if (err) {
-              return(err, null);
-            } else {
-              return(null, newPath);
-            }
-          });
-        });
-      })
-    })
+    });
   }
   getFileName(req) {
     return new Promise((resolve, reject) => {
@@ -62,7 +63,7 @@ class goodsService {
         if (err) {
           reject(err)
         } else {
-          resolve(fields.goodsId)
+          resolve(fields.tempId)
         }
       })
     })
