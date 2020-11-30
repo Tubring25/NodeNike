efbdc650023207199567ad924216ba297cd32431
Sameen <tubring25@gmail.com>
2020-11-30 21:22:34 +0800

    vuex

---
+++ vue_nikeshop_admin/src/store/modules/goodsAllTypes.js
@@ -0,0 +1,13 @@
+const state = {
+  base_type: [],
+  gender_type: [],
+  brands_type: [],
+  sports_type: []
+}
+const mutations = {}
+
+const actions = {}
+
+export default {
+  state, mutations, actions
+}
